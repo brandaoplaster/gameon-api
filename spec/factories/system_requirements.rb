d04a683f5fceb282feefea6e55ed85b0761ab4f3
brandaoplaster <brandaoplaster@gmail.com>
2020-11-14 18:59:42 -0300

    Factories, tests and model adjustments

--- spec/factories/system_requirements.rb
+++ spec/factories/system_requirements.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :system_requirement do
     sequence(:name) { |n| "Basic #{n}" }
-    operation_system { Faker::Computer.os }
+    operational_system { Faker::Computer.os }
     storage { "500GB" }
     processor { "INTEL i7" }
     memory { "8GB" }
