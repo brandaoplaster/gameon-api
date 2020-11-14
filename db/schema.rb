92b151e81ccc38595bd5c5d7c531803b6201313c
brandaoplaster <brandaoplaster@gmail.com>
2020-11-14 00:04:05 -0300

    Creating model system requirement and generate migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_14_020528) do
+ActiveRecord::Schema.define(version: 2020_11_14_030316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "system_requirements", force: :cascade do |t|
+    t.string "name"
+    t.string "operation_system"
+    t.string "storage"
+    t.string "processor"
+    t.string "memory"
+    t.string "video_board"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
---
+++ spec/factories/system_requirements.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :system_requirement do
+    name { "MyString" }
+    operation_system { "MyString" }
+    storage { "MyString" }
+    processor { "MyString" }
+    memory { "MyString" }
+    video_board { "MyString" }
+  end
+end
