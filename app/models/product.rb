c5add0c787fadbd0ab153cf0e4755056ace2b305
brandaoplaster <brandaoplaster@gmail.com>
2020-11-20 18:56:07 -0300

    chore: adding image and validation presence in model product

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,9 @@ class Product < ApplicationRecord
   belongs_to :productable, polymorphic: true
   has_many :product_categories, dependent: :destroy
   has_many :categories, through: :product_categories
+  has_one_attached :image
 
+  validates :image, presence: true
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
