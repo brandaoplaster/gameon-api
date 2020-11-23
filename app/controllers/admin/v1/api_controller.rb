ca25a0571bf82efc2a80aeb662e9b26df5cd1e23
brandaoplaster <brandaoplaster@gmail.com>
2020-11-22 23:40:47 -0300

    chore: adding access restriction

--- app/controllers/admin/v1/api_controller.rb
+++ app/controllers/admin/v1/api_controller.rb
@@ -1,5 +1,7 @@
 module Admin::V1
   class ApiController < ApplicationController
+    class ForbiddenAccess < StandardError; end
+
     include Authenticatable
 
     def render_error(message: nil, fields: nil, status: :unprocessable_entity)
@@ -7,6 +9,18 @@ module Admin::V1
       errors['fields'] = fields if fields.present?
       errors['message'] = message if message.present?
       render json: { errors: errors }, status: status
+    end
+
+    rescue_from ForbiddenAccess do 
+      render_error(message: "Forbidden access", status: :forbidden)
+    end
+    
+    before_action :restrict_access_for_admin!
+
+    private
+
+    def restrict_access_for_admin!
+      raise ForbiddenAccess unless current_user.admin?
     end
   end
 end
