93ea36701308fa2742bfd53b8c72d9f0df6a7fee
brandaoplaster <brandaoplaster@gmail.com>
2020-11-28 22:36:09 -0300

    feat: adding action create and testing for controller system requirements

--- spec/requests/admin/v1/system_requeriments/admin_requests_spec.rb
+++ spec/requests/admin/v1/system_requeriments/admin_requests_spec.rb
@@ -17,4 +17,33 @@ RSpec.describe "Admin V1: SystemRequirements as :admin", type: :request do
       expect(response).to have_http_status(:ok)
     end
   end
+
+  context "POST /system_requirements" do
+    let(:url) { "/admin/v1/system_requirements" }
+
+    context "with valid params" do
+      let(:system_requirement_params) { { system_requirement: attributes_for(:system_requirement) }.to_json }
+    
+      it "adds a new system requirement" do
+        expect do
+          post url, headers: auth_header(user), params: system_requirement_params
+        end.to change(SystemRequirement, :count).by(1)
+      end
+
+      it "returns last added system riquirement" do
+        post url, headers: auth_header(user), params: system_requirement_params
+        expected_system_requirement = SystemRequirement.last.as_json(only: %i(id name operational_system storage processor memory video_board))
+        expect(body_json['system_requirement']).to eq(expected_system_requirement)
+      end
+
+      it "returns success status" do
+        post url, headers: auth_header(user), params: system_requirement_params
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "with invalid params" do
+
+    end
+  end
 end
