255c361353ff9a2cabe54d44026997cdbf844c47
brandaoplaster <brandaoplaster@gmail.com>
2021-02-04 23:47:09 -0300

    chore: creating initial structure

---
+++ spec/requests/admin/v1/licenses/admin_requests_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "Admin V1 Licenses as :admin", type: :request do
+  
+end
---
+++ spec/requests/admin/v1/licenses/unauthenticated_requests_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe "Admin V1 Licenses as :client", type: :request do
+
+  
+end
