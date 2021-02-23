3c12c42628756127c5484e012976c994ab2853be
brandaoplaster <brandaoplaster@gmail.com>
2021-02-23 00:27:05 -0300

    chore: [WIP] tests for admin request

--- spec/requests/admin/v1/licenses/admin_requests_spec.rb
+++ spec/requests/admin/v1/licenses/admin_requests_spec.rb
@@ -64,4 +64,67 @@ RSpec.describe "Admin V1 Licenses as :admin", type: :request do
       end
     end
   end
+
+  context 'POST /games/:game_id/licenses' do
+    let(:url) { "/admin/v1/games/#{game.id}/licenses" }
+
+    context 'with valid params' do
+      let(:license_params) { { license: attributes_for(:license) }.to_json }
+
+      it 'adds a new license' do
+        expect do
+          post url, headers: auth_header(user), params: license_params
+        end.to change(License,  :count).by(1)
+      end
+
+      it 'returns last added license' do
+        post url, headers: auth_header(user), params: license_params
+        expected_license = License.last.as_json(only: %i(id key platform status game_id))
+        expect(body_json['license']).to eq expected_license
+      end
+
+      it 'returns success status' do
+        pots url, headers: auth_header(user), params: license_params
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'with invalid params' do
+      let(:license_invalid_params) do
+        { license: attributes_for(:license, key: nil) }.to_json
+      end
+
+      it 'does not add a new license' do
+        expect do
+          post url, headers: auth_header(user), params: license_invalid_params
+        end.to change(License, :count)
+      end
+
+      it 'returns error message' do
+        post url, headers: auth_header(user), params: license_invalid_params
+        expect(body_json['errors']['fields']).to have_key('key')
+      end
+
+      it 'returns unprocessable entity status' do
+        post url, params: auth_header(user), params: license_invalid_params
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
+  context 'GET /licenses/:id' do
+    let(:license) { create(:license) }
+    let(:url) { "/admin/v1/licenses/#{license.id}" }
+
+    it 'returns requested license' do
+      get url, headers: auth_header(user)
+      expected_license = license.as_json(only: %i(id key platform status game_id))
+      expect(body_json['license']).to eq expected_license
+    end
+
+    it 'returns success status' do
+      get url, headers: auth_header(user)
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end
